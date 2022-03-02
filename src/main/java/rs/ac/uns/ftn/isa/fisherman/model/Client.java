f7b8bfd443dd25fd4016510dfb62d58613c27a9f
Aleksa Stojić <aleksa.stojic19@uns.ac.rs>
2022-03-02 18:05:02 +0100

    fix: Deleted unnecessary field from Client model

--- src/main/java/rs/ac/uns/ftn/isa/fisherman/model/Client.java
+++ src/main/java/rs/ac/uns/ftn/isa/fisherman/model/Client.java
@@ -8,22 +8,15 @@ import javax.persistence.Entity;
 public class Client extends User{
 
     private static String roleApp = "ROLE_CLIENT";
-    private String registrationReason;
+
     public Client() {
 
     }
 
-    public Client(Long id, String name, String lastName, String username, String password, String phoneNum, Address address ,String registrationReason) {
+    public Client(Long id, String name, String lastName, String username, String password, String phoneNum, Address address) {
         super(id, name, lastName, username, password, phoneNum, address);
-        this.registrationReason=registrationReason;
     }
 
-    public String getRegistrationReason() {
-        return registrationReason;
-    }
-    public void setRegistrationReason(String registrationReason) {
-        this.registrationReason = registrationReason;
-    }
     @Override
     public String getRoleApp() {
         return roleApp;
